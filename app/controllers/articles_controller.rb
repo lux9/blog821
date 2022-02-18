5c259898a17fb729d36244245fa2f3093fce3ec9
lux <luischn94@gmail.com>
2022-02-18 10:57:17 -0300

    update articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,13 +24,11 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     # respond_to do |format|
-      if @article.save
-        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.save
+      redirect_to article_url(@article)
+    else
+      render :new
+    end
     # end
   end
 
